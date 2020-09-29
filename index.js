7700935b339545dbbcb0351e2fa8c400cb777dbf
Chris Johnson <c.johnson@bamboonine.co.uk>
2020-09-29 09:13:08 +0100

    First version.

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = async function (url) {
+  const beautify = require('js-beautify').html
+  const jsdiff = require('diff')
+  const puppeteer = require('puppeteer')
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  let htmlraw
+  page.on('response', async response => {
+    if (![301, 302, 307].includes(response.status())) {
+      if (!htmlraw) {
+        htmlraw = await response.text()
+      }
+    }
+  })
+
+  await page.goto(url, { waitUntil: 'networkidle2' })
+  const htmlparsed = await page.content()
+  await browser.close()
+
+  const input = jsdiff.createPatch(
+    'diff',
+    beautify(htmlraw, { indent_size: 2, space_in_empty_paren: true }),
+    beautify(htmlparsed, { indent_size: 2, space_in_empty_paren: true })
+  )
+
+  return input
+}
